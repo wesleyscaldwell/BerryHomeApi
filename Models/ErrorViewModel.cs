01d289dd2bd85ba5e93e2a0ce6d614ed89d58d56
Wesley Caldwell <wesley@caldwellgroup.us>
2020-05-02 11:00:44 -0500

    Init Checkin

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BerryHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
